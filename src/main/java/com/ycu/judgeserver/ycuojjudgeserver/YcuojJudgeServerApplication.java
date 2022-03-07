e64356a5c5f709b5fbfd746723f801a71efb83e6
Xuxiaoboaaa <1037366429@qq.com>
2022-03-07 18:52:17 +0800

    Initial commit

---
+++ src/main/java/com/ycu/judgeserver/ycuojjudgeserver/YcuojJudgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.ycu.judgeserver.ycuojjudgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcuojJudgeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcuojJudgeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ycu/judgeserver/ycuojjudgeserver/YcuojJudgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycu.judgeserver.ycuojjudgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcuojJudgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
